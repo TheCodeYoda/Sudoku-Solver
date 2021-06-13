2d2220202602b03cc34e11983ced4e2810826212
TheCodeYoda <smurf61988@gmail.com>
2021-06-13 21:07:05 +0530

    struct based code

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,30 @@
-mod SudokuSolve;
+mod sudoku_solve;
 
-use text_io::scan;
 use text_io::read;
+
+fn read_board(n: usize) -> Vec<Vec<char>> {
+    let mut board: Vec<Vec<char>> = Vec::new();
+    for _ in 0..n {
+        let mut row: Vec<char> = Vec::new();
+        for _ in 0..n {
+            let elem: char = read!();
+            row.push(elem);
+        }
+        board.push(row);
+    }
+    return board;
+}
 
 fn main() {
     println!("Enter size of grid followed by the grid....");
 
-    let n:usize = read!();
-    let mut board: Vec<Vec<char>> = Vec::new();
+    let n: usize = read!();
+    let board = read_board(n);
 
-    for i in 0..n {
-	let mut row: Vec<char> = Vec::new();
-	for j in 0..n {
-	    let elem:char = read!();
-	    row.push(elem);
-	}
-	board.push(row);
-    }
+    let mut instance = sudoku_solve::Sudoku { n, board };
     println!("\nBefore solving...");
-    SudokuSolve::display(&board,n);
-    SudokuSolve::solve_board(n,&mut board);
+    instance.display();
+    instance.solve_board();
     println!("\nAfter solving...");
-    SudokuSolve::display(&board,n);
-	
+    instance.display();
 }
